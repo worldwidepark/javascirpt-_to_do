4c86efc518cb19204e58b30a000169d104f219b0
worldwidepark <83998627+worldwidepark@users.noreply.github.com>
2022-08-23 21:20:19 +0900

    delete, edit, confirm

--- toDoList.js
+++ toDoList.js
@@ -25,11 +25,13 @@ function is_checked() {
   uncheckedCounts.innerText = `未完了:${unchecked.length}`;
 }
 
-function myFunction() {
-  // Create an "li" node:
-  const name = document.getElementById("name").value;
-  const node = document.getElementById("checkbox");
 
+function myFunction(targetValue) {
+  // Create an "li" node:
+  const inputValue = document.getElementById(targetValue).value;
+  const node = document.getElementById("checkbox");
+  const checkForm = document.createElement("form");
+  
   // Create a text node:
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
@@ -40,11 +42,90 @@ function myFunction() {
   // checkbox.id=
 
   const label = document.createElement("label");
-  label.appendChild(document.createTextNode(name));
+  label.appendChild(document.createTextNode(inputValue));
   // Append the text node to the "li" node:
 
-  node.appendChild(checkbox);
-  node.appendChild(label);
+  const editButton = document.createElement("button");
+  const deleteButton = document.createElement("button");
+  editButton.type = 'button';
+  editButton.innerText = 'edit';
+  editButton.setAttribute("onclick", "edit(event)");
+  
+  deleteButton.type = 'button';
+  deleteButton.innerText = 'delete';
+  deleteButton.setAttribute("onclick","deleteForm(event)");
+  
+  checkForm.appendChild(checkbox);
+  checkForm.appendChild(label);
+  checkForm.appendChild(editButton);
+  checkForm.appendChild(deleteButton);
+  
+  node.appendChild(checkForm);
+  is_checked();
   // Append the "li" node to the list:
   // document.getElementById("myList").appendChild(node);
 }
+
+function edit(event){
+  const parent = event.target.parentElement;
+  while (parent.hasChildNodes()){
+    parent.removeChild(parent.firstChild);
+  }
+  const input = document.createElement("input");
+  input.type = "text" 
+  input.id = "edit"
+  const editButton = document.createElement("button");
+  editButton.type = 'button';
+  editButton.innerText = 'edit';
+  editButton.setAttribute("onclick", "myFunction2(event)");
+
+  parent.appendChild(input);
+  parent.appendChild(editButton); 
+
+}
+function myFunction2(event) {
+  const parent = event.target.parentElement;
+  const inputValue = document.getElementById("edit").value;
+  while (parent.hasChildNodes()){
+    parent.removeChild(parent.firstChild);
+  }
+  
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.name = "hobby";
+  checkbox.value = "done";
+  checkbox.setAttribute("onclick", "is_checked()");
+
+
+  const label = document.createElement("label");
+  label.appendChild(document.createTextNode(inputValue));
+
+  const editButton = document.createElement("button");
+  const deleteButton = document.createElement("button");
+  editButton.type = 'button';
+  editButton.innerText = 'edit';
+  editButton.setAttribute("onclick", "edit(event)");
+  
+  deleteButton.type = 'button';
+  deleteButton.innerText = 'delete';
+  deleteButton.setAttribute("onclick", "deleteForm(event)");
+  
+  parent.appendChild(checkbox);
+  parent.appendChild(label);
+  parent.appendChild(editButton);
+  parent.appendChild(deleteButton);
+  
+  is_checked();
+  }
+
+  function deleteForm(event){
+    let flag = confirm("Are you sure to delete this list");
+    if(flag == true){
+      const parent = event.target.parentElement;
+      parent.remove();
+      is_checked();
+    }else{
+      return;
+    }
+  }
+
