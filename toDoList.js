f67ddabd5b2e4af2a435c9bea4418ab40c0f15a0
worldwidepark <qkrguddn1066@naver.com>
2022-08-25 01:07:29 +0900

    firstcheck

--- toDoList.js
+++ toDoList.js
@@ -1,30 +1,31 @@
-function is_checked() {
-  let wholeCounts = document.getElementById("wholeCounts");
-  let checkedCounts = document.getElementById("checkedCounts");
-  let uncheckedCounts = document.getElementById("uncheckedCounts");
+const isChecked = () => {
+  const wholeCounts = document.getElementById("wholeCounts");
+  const checkedCounts = document.getElementById("checkedCounts");
+  const uncheckedCounts = document.getElementById("uncheckedCounts");
 
-  let checked = document.querySelectorAll("input[name=todolist]:checked");
-  let unchecked = document.querySelectorAll(
+  const checked = document.querySelectorAll("input[name=todolist]:checked");
+  const unchecked = document.querySelectorAll(
     "input[name=todolist]:not(:checked)"
   );
 
-  wholeCounts.innerText = `全てのタスク:${checked.length + unchecked.length}`;
-  checkedCounts.innerText = `完了済:${checked.length}`;
-  uncheckedCounts.innerText = `未完了:${unchecked.length}`;
-}
+  wholeCounts.innerText = String(checked.length + unchecked.length);
+  checkedCounts.innerText = String(checked.length);
+  uncheckedCounts.innerText = String(unchecked.length);
+};
 
-function createToDoList(inputValue, checkForm) {
-  let checkbox = document.createElement("input");
+const createToDoList = (inputValue, checkForm, checked) => {
+  const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.name = "todolist";
   checkbox.value = "done";
-  checkbox.setAttribute("onclick", "is_checked()");
+  checkbox.setAttribute("onclick", "isChecked()");
+  checkbox.checked = checked;
 
-  let label = document.createElement("label");
+  const label = document.createElement("label");
   label.appendChild(document.createTextNode(inputValue));
 
-  let editButton = document.createElement("button");
-  let deleteButton = document.createElement("button");
+  const editButton = document.createElement("button");
+  const deleteButton = document.createElement("button");
   editButton.type = "button";
   editButton.innerText = "edit";
   editButton.setAttribute("onclick", "makeEditInput(event)");
@@ -39,31 +40,32 @@ function createToDoList(inputValue, checkForm) {
   checkForm.appendChild(label);
   checkForm.appendChild(editButton);
   checkForm.appendChild(deleteButton);
-}
+};
 
-function makeToDoList(targetValue) {
-  const inputValue = document.getElementById(targetValue).value;
-  if (inputValue.length == 0) {
+const makeToDoList = () => {
+  const inputValue = document.getElementById("inputToDo").value;
+  if (inputValue.length === 0) {
     alert("Please Put something.");
   } else {
     const node = document.getElementById("checkbox");
-    if (node.children[0].innerText == "Nothing to do. Put something.") {
+    if (node.children[0].innerText === "Nothing to do. Put something.") {
       node.children[0].innerText = "";
     }
     const checkForm = document.createElement("form");
     checkForm.id = "checkForm";
     createToDoList(inputValue, checkForm);
     node.appendChild(checkForm);
-    is_checked();
+    isChecked();
     document.getElementById("inputToDoForm").reset();
   }
-}
+};
 
-function makeEditInput(event) {
-  let parent = event.target.parentElement;
-  let editValue = parent.childNodes[1].textContent;
-  console.log(editValue);
-  let input = document.createElement("input");
+const makeEditInput = (event) => {
+  const parent = event.target.parentElement;
+  const editValue = parent.childNodes[1].textContent;
+  //checkboxがチェックされているかを確認
+  const checked = parent.childNodes[0].checked;
+  const input = document.createElement("input");
   input.type = "text";
   input.id = "edit";
   input.value = editValue;
@@ -71,16 +73,20 @@ function makeEditInput(event) {
   while (parent.hasChildNodes()) {
     parent.removeChild(parent.firstChild);
   }
-  let editButton = document.createElement("button");
+  const editButton = document.createElement("button");
   editButton.type = "button";
   editButton.innerText = "edit";
   editButton.id = "editButton";
-  editButton.setAttribute("onclick", "inputEditedToDoList(event)");
+  //checkboxのチェックの結果を引数として渡す。
+  editButton.setAttribute(
+    "onclick",
+    `inputEditedToDoList(event, checked = ${checked} )`
+  );
 
   parent.appendChild(input);
   parent.appendChild(editButton);
-}
-function inputEditedToDoList(event) {
+};
+const inputEditedToDoList = (event, checked) => {
   const inputValue = document.getElementById("edit").value;
   if (inputValue.length == 0) {
     alert("Please Put something.");
@@ -90,21 +96,24 @@ function inputEditedToDoList(event) {
     while (parent.hasChildNodes()) {
       parent.removeChild(parent.firstChild);
     }
-    createToDoList(inputValue, parent);
-    is_checked();
+    //チェック結果の引数を渡す。
+    createToDoList(inputValue, parent, checked);
   }
-}
-function deleteToDoList(event) {
-  let flag = confirm("Are you sure to delete this list?");
-  if (flag == true) {
-    let parent = event.target.parentElement;
-    let checkbox = parent.parentElement;
+};
+const deleteToDoList = (event) => {
+  const flag = confirm("Are you sure to delete this list?");
+  if (flag) {
+    const parent = event.target.parentElement;
+    const checkbox = parent.parentElement;
     parent.remove();
-    is_checked();
-    if (checkbox.childElementCount == 1) {
+    isChecked();
+    if (checkbox.childElementCount === 1) {
       checkbox.children[0].innerText = "Nothing to do. Put something.";
     }
   } else {
     return;
   }
-}
+};
+
+const inputBtn = document.getElementById("inputButton");
+inputBtn.addEventListener("click", makeToDoList);
